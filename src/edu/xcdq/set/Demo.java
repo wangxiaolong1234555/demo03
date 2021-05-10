dabac9c64890648e855a6faa6a24baf43bffa96e
wangxiaolong <3355137186@qq.com>
2021-05-10 08:27:27 +0800

    课堂
    Signed-off-by: wangxiaolong <3355137186@qq.com>

--- src/edu/xcdq/set/Demo.java
+++ src/edu/xcdq/set/Demo.java
@@ -2,6 +2,6 @@ package edu.xcdq.set;
 
 public class Demo {
     public static void main(String[] args) {
-        String[] str = {"中文",}
+        String[] str = {"中文",};
     }
 }
---
+++ src/edu/xcdq/set/Demo01.java
@@ -0,0 +1,12 @@
+package edu.xcdq.set;
+
+import java.util.HashMap;
+
+public class Demo01 {
+    public static void main(String[] args) {
+        HashMap<> map = new HashMap<>();
+        map.put("1", "A");
+        map.put("2", "B");
+        map.put("3", "4");
+    }
+}
