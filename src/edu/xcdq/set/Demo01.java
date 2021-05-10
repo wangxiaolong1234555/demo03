b24880d2619c4110cb5d5b169e3a8bf7fdb90dde
wangxiaolong <3355137186@qq.com>
2021-05-10 08:33:55 +0800

    课堂
    Signed-off-by: wangxiaolong <3355137186@qq.com>

--- src/edu/xcdq/set/Demo01.java
+++ src/edu/xcdq/set/Demo01.java
@@ -1,12 +1,20 @@
 package edu.xcdq.set;
 
-import java.util.HashMap;
+import java.util.*;
 
 public class Demo01 {
     public static void main(String[] args) {
-        HashMap<> map = new HashMap<>();
+        HashMap<String, String> map = new HashMap<>();
         map.put("1", "A");
         map.put("2", "B");
-        map.put("3", "4");
+        map.put("3", "C");
+        Set<String> keys = map.keySet();
+        System.out.println(keys);
+        ArrayList<String> arrayListKeys = new ArrayList<>(keys);
+        System.out.println(arrayListKeys);
+        Collection<String> v = map.values();
+        System.out.println(v);
+        LinkedList<String> llisttv = new LinkedList<>(v);
+        System.out.println(llisttv);
     }
 }
